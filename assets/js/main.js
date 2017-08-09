c7327cbb731bf2ab7ca4e757168dd8c4bde6183f
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-08-09 10:48:02 -0500

    Love Panda Terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var fotos = document.getElementById('cuadros');
+
+for(var i = 0; i<pictures.length ; i++){
+	var img = document.createElement("img");
+	var atri = img.setAttribute("src", pictures[i]);
+	img.setAttribute("class", "cuadros");
+	fotos.appendChild(img);
+}
